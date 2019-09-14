5555f3f5a528ecf9ed838889e472cc7589fc3121
FreedomWriter <natalienetworks@gmail.com>
2019-09-13 20:38:08 -0500

    tab component added

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics');
+
+//get the data
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((results) => {
+        let data = results.data.topics;
+        data.forEach((item) => {
+            const tabs = Tabs(item);
+            topics.appendChild(tabs);
+        });
+    });
+
+//create the component function
+function Tabs (topic) {
+    //create the element
+    let tab = document.createElement('div');
+    //set the content
+    tab.textContent = topic;
+    //styel the element
+    tab.classList.add('tab');
+    return tab;
+}
